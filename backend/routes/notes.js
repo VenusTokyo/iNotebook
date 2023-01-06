288440114342f9510a60ec9cb3c3b1e94a4d7aa9
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-01-06 12:53:52 +0530

    adding notes route refactor'

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,38 +1,46 @@
-const express=require('express')
-const router =express.Router()
-const fetchUser =require('../middleware/fetchUser')
+const express = require('express')
+const router = express.Router()
+const fetchUser = require('../middleware/fetchUser')
 const { body, validationResult } = require('express-validator');
 
-const Notes = require('../models/Notes')
+const Note = require('../models/Note')
 // route 1 to get all notes of a user
-router.get('/fetchallnotes',fetchUser,
-async (req,res)=>{
-    const notes= await Notes.find({user:req.user.id})
-    res.json(notes)
-})
-// route 2 adding new notes
-router.get('/addnote',fetchUser,[
-body('title', 'Enter a valid title').isLength({ min: 2 }),
-body('description', 'Description must be atleat 5 charecters').isLength({ min: 5 })
-],
-async (req,res)=>{
-    try {
-        
-        const {title, description, tag,}=req.body
-        const errors = validationResult(req);
-        
-        if (!errors.isEmpty()) 
-        {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        const note = new Note({
-            title, description, tag, user:req.user.id
-        })
-        const saveNote = await note.save()
-        res.json(saveNotes)
-    } catch (error) {
-        
-    }
-})
+router.get('/fetchallnotes', fetchUser,
+    async (req, res) => {
+        try {
 
-module.exports=router
+            const note= await Note.find({ user: req.user.id })
+            res.json(note)
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("Internal Server Error")
+        }
+    })
+
+
+
+// route 2 adding new notes
+router.post('/addnote', fetchUser, [
+    body('title', 'Enter a valid title').isLength({ min: 2 }),
+    body('description', 'Description must be atleat 5 charecters').isLength({ min: 5 })
+],
+    async (req, res) => {
+        try {
+
+            const { title, description, tag, } = req.body
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const saveNote = await note.save()
+            res.json(saveNote)
+        } catch (error) {
+
+        }
+    })
+
+module.exports = router
