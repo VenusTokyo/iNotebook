c1e25833f1f63606e3fac8a85e9b0d914890ea2c
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-01-06 12:32:54 +0530

    aaded routes to fetchnotes and add notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,7 +1,38 @@
 const express=require('express')
 const router =express.Router()
-router.get('/',(req,res)=>{
-    
-    res.json([])
+const fetchUser =require('../middleware/fetchUser')
+const { body, validationResult } = require('express-validator');
+
+const Notes = require('../models/Notes')
+// route 1 to get all notes of a user
+router.get('/fetchallnotes',fetchUser,
+async (req,res)=>{
+    const notes= await Notes.find({user:req.user.id})
+    res.json(notes)
 })
+// route 2 adding new notes
+router.get('/addnote',fetchUser,[
+body('title', 'Enter a valid title').isLength({ min: 2 }),
+body('description', 'Description must be atleat 5 charecters').isLength({ min: 5 })
+],
+async (req,res)=>{
+    try {
+        
+        const {title, description, tag,}=req.body
+        const errors = validationResult(req);
+        
+        if (!errors.isEmpty()) 
+        {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title, description, tag, user:req.user.id
+        })
+        const saveNote = await note.save()
+        res.json(saveNotes)
+    } catch (error) {
+        
+    }
+})
+
 module.exports=router
